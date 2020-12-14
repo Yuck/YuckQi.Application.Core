b5156cc4213d1a0372fc2d1142d8e857a696c0ec
Kevin J Lambert <kevin.lambert@poconosystems.com>
2020-12-14 18:22:42 -0500

    Modified search query to be immutable

--- src/YuckQi.Application.Core/Abstract/SearchQueryBase.cs
+++ src/YuckQi.Application.Core/Abstract/SearchQueryBase.cs
@@ -1,14 +1,14 @@
 ﻿using MediatR;
-using YuckQi.Domain.Application.Queries.Abstract;
 using YuckQi.Domain.Application.Queries.Results;
 using YuckQi.Domain.Entities.Abstract;
 using YuckQi.Domain.Validation;
 
 namespace YuckQi.Application.Core.Abstract
 {
-    public abstract class SearchQueryBase<TEntity, TKey> : IRequest<Result<Page<TEntity, TKey>>>, ISearchQuery where TEntity : class, IEntity<TKey> where TKey : struct
+    public abstract class SearchQueryBase<TEntity, TKey> : Page, IRequest<Result<Page<TEntity, TKey>>> where TEntity : class, IEntity<TKey> where TKey : struct
     {
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
+        protected SearchQueryBase(int number, int size) : base(number, size)
+        {
+        }
     }
 }
--- src/YuckQi.Application.Core/TypeSearchQuery.cs
+++ src/YuckQi.Application.Core/TypeSearchQuery.cs
@@ -5,7 +5,19 @@ namespace YuckQi.Application.Core
 {
     public class TypeSearchQuery<TEntity, TKey> : SearchQueryBase<TEntity, TKey> where TEntity : TypeEntityBase<TKey> where TKey : struct
     {
+        #region Properties
+
         public string Code { get; set; }
         public string Name { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public TypeSearchQuery(int number, int size) : base(number, size)
+        {
+        }
+
+        #endregion
     }
 }
