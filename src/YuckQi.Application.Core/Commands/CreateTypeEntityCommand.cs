0ccd0e430f5cc0519ef06a70e74d23137e304d93
Kevin J Lambert <kevin.lambert@poconosystems.com>
2021-02-03 16:52:39 -0500

    Implementing a baseline for creating type entities

--- src/YuckQi.Application.Core/Commands/CreateTypeEntityCommand.cs
+++ src/YuckQi.Application.Core/Commands/CreateTypeEntityCommand.cs
@@ -7,5 +7,22 @@ namespace YuckQi.Application.Core.Commands
 {
     public class CreateTypeEntityCommand<TTypeEntity, TKey> : IRequest<Result<TTypeEntity>> where TTypeEntity : IEntity<TKey>, IType where TKey : struct
     {
+        #region Properties
+
+        public string Name { get; }
+        public string ShortName { get; }
+
+        #endregion
+
+
+        #region Constructors
+
+        public CreateTypeEntityCommand(string name, string shortName)
+        {
+            Name = name;
+            ShortName = shortName;
+        }
+
+        #endregion
     }
 }
