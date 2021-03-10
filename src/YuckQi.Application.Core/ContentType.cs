5bdfd774ea3d748d4ce42270f6faff4bf2e46f12
Kevin J Lambert <kevin.lambert@poconosystems.com>
2021-03-10 07:29:35 -0500

    Added a couple useful constants/extensions, which need a proper home

---
+++ src/YuckQi.Application.Core/ContentType.cs
@@ -0,0 +1,10 @@
+﻿using MimeContentType = System.Net.Mime.ContentType;
+
+namespace YuckQi.Application.Core
+{
+    // TODO: Where should this really go? It's specific to one type of host (i.e. web host) rather than a generic application
+    public static class ContentType
+    {
+        public static MimeContentType ApplicationJson = new MimeContentType("application/json");
+    }
+}
---
+++ src/YuckQi.Application.Core/ObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace YuckQi.Application.Core
+{
+    // TODO: Where should this really go? Maybe a folder/namespace for Extensions/Serialization (or Serialization/Extensions)?
+    public static class ObjectExtensions // TODO: Name this ObjectSerializerExtensions?
+                                         // But should this contain only JSON or also XML? Probably just JSON, and then name it Serialization/Json/Extensions perhaps
+    {
+        public static string ToJson<T>(T thing)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
